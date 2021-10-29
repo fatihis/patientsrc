c5837db9764e0eb4683b82b7e91193c93cc08d21
fatihis99@hotmail.com <isk.fatih1@gmail.com>
2021-10-29 17:16:40 +0300

    Add RadioButton

---
+++ components/atoms/RadioButton/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {styles} from './styles';
+import {Text, TouchableOpacity, View} from 'react-native';
+
+export const RadioButton = props => {
+  return (
+    <TouchableOpacity
+      style={styles.radioButtonContainer}
+      onPress={() => props.onPress()}>
+      <View
+        style={[
+          {
+            height: 24,
+            width: 24,
+            borderRadius: 12,
+            borderWidth: 2,
+            borderColor: '#000',
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginRight: 10,
+          },
+        ]}>
+        {props.selected ? (
+          <View
+            style={{
+              height: 12,
+              width: 12,
+              borderRadius: 6,
+              backgroundColor: '#200fd9',
+            }}
+          />
+        ) : null}
+      </View>
+      <Text style={styles.radioLabel}>{props.title}</Text>
+    </TouchableOpacity>
+  );
+};
---
+++ components/atoms/RadioButton/styles.js
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  radioButtonContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  radioLabel: {
+    alignSelf: 'center',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    marginRight: 10,
+  },
+});
