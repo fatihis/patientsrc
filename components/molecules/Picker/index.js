723b84b2810fb42be323228019fce37a5bd82e51
fatihis99@hotmail.com <isk.fatih1@gmail.com>
2021-10-29 15:29:46 +0300

    Add Picker component

--- components/molecules/Picker/index.js
+++ components/molecules/Picker/index.js
@@ -1,17 +1,22 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import {PickerItem} from '../../atoms/PickerItem';
 import {Picker} from '@react-native-picker/picker';
 import {styles} from './styles';
+import {PatientContext} from '../../../contexts/PatientContext/PatientContext';
 
 export const DropdownPicker = () => {
-  const [selectedLanguage, setSelectedLanguage] = useState();
-
+  const patientCtx = useContext(PatientContext);
   return (
     <Picker
-      selectedValue={selectedLanguage}
-      onValueChange={(itemValue, itemIndex) => setSelectedLanguage(itemValue)}>
-      <PickerItem label="s" value="j" />
-      <PickerItem label="s" value="j" />
+      style={styles.pickerContainer}
+      selectedValue={patientCtx.currentFilter}
+      onValueChange={(itemValue, itemIndex) =>
+        patientCtx.setCurrentFilter(itemValue)
+      }>
+      <PickerItem label="Name" value="name" />
+      <PickerItem label="Given" value="given" />
+      <PickerItem label="Family" value="family" />
+      <PickerItem label="National ID" value="nationalid" />
     </Picker>
   );
 };
