8df53b7dbb1cb555f62f87b89d512400ccd6798f
fatihis99@hotmail.com <isk.fatih1@gmail.com>
2021-10-29 17:17:33 +0300

    Add RadioGroup to SearchBox component

--- components/organisms/SearchBox/index.js
+++ components/organisms/SearchBox/index.js
@@ -1,15 +1,19 @@
 import React, {useContext} from 'react';
 import {View, Button} from 'react-native';
 import {PatientContext} from '../../../contexts/PatientContext/PatientContext';
+import {RadioButton} from '../../atoms/RadioButton';
 import {DropdownPicker} from '../../molecules/Picker';
+import {RadioGroup} from '../../molecules/RadioGroup';
 import {SearchInput} from '../../molecules/SearchInput';
 
-const SearchBox = props => {
+const SearchBox = () => {
   const patientCtx = useContext(PatientContext);
   return (
     <View>
       <SearchInput onChange={e => patientCtx.setSearchString(e)} />
       <DropdownPicker />
+      <RadioGroup optionArray={patientCtx.asteriskRadioArray} />
+
       <Button title="Search" onPress={() => patientCtx.addNewSearchExp()} />
     </View>
   );
