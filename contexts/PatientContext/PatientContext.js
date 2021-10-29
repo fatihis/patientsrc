8e3e1c49c0ab87ff2113421de8b3da9597264418
fatihis99@hotmail.com <isk.fatih1@gmail.com>
2021-10-29 15:30:34 +0300

    Add PatientContext

---
+++ contexts/PatientContext/PatientContext.js
@@ -0,0 +1,122 @@
+import React, {createContext, useEffect, useState} from 'react';
+import {encodeQueryData, parseAddress} from '../../utils/helpers';
+export const PatientContext = createContext();
+export const PatientContextProvider = ({children}) => {
+  const [searchString, setSearchString] = useState('');
+  const [currentFilter, setCurrentFilter] = useState('name');
+  const [searchExpression, setSearchExpression] = useState('');
+  const [patientList, setPatientList] = useState([]);
+  const [tagObj, setTagObj] = useState({
+    name: '',
+    nationalid: '',
+    given: '',
+    family: '',
+  });
+  const [isCooldown, setIsCooldown] = useState(false);
+  const [queryCounter, setQueryCounter] = useState(0);
+  const [displayData, setDisplayData] = useState(0);
+
+  useEffect(() => {
+    if (searchString.length >= 2) {
+      fetchPatientData();
+    }
+  }, [searchExpression]);
+
+  const addNewSearchExp = () => {
+    if (searchString.length >= 2 && isCooldown != true) {
+      updateTags();
+      setConcatQueryExpression();
+      cooldownSearch(3000);
+    } else if (searchString.length < 2) {
+      alert('Please type more than 2 character/digit at least');
+    } else if (isCooldown === true) {
+      alert('Slow down! Please try again in 5 sec.');
+    }
+  };
+
+  const updateTags = () => {
+    var mutableTagObj = tagObj;
+    mutableTagObj[currentFilter] = searchString;
+    setTagObj(mutableTagObj);
+  };
+  const setConcatQueryExpression = () => {
+    const queryString = '?' + encodeQueryData(tagObj);
+    console.log(queryString);
+    setSearchExpression(queryString);
+  };
+
+  const constructDisplayData = patientArray => {
+    var mutableDisplayDataArray = [];
+    if (patientArray != null) {
+      patientArray.map(patient => {
+        var patientData = {
+          id: patient.id,
+          name: patient.resource.name[0].given[0],
+          familyName: patient.resource.name[0].family,
+          nationalId: patient.resource.id,
+          gender: patient.resource.gender,
+          phone: patient.resource.telecom[0].value,
+          address: parseAddress(patient.resource.address[0]),
+        };
+        mutableDisplayDataArray.push(patientData);
+      });
+      setDisplayData(mutableDisplayDataArray);
+    }
+  };
+
+  const cooldownSearch = millis => {
+    setQueryCounter(prev => prev + 1);
+    if (queryCounter > 2) {
+      setIsCooldown(true);
+      setTimeout(() => {
+        setIsCooldown(false);
+        setQueryCounter(0);
+      }, millis);
+    }
+  };
+
+  const fetchPatientData = async () => {
+    var myHeaders = new Headers();
+    myHeaders.append('Content-Type', ' application/fhir+json');
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow',
+    };
+    const response =
+      searchExpression != ''
+        ? await fetch(
+            'http://fhir.imagerad.com/dummy/Patient/' + searchExpression,
+            requestOptions,
+          )
+            .then(res => res.json())
+            .then(data => {
+              console.log(data);
+              constructDisplayData(data.entry);
+              return data;
+            })
+        : null;
+    setPatientList(response.entry); //null point exc
+  };
+
+  const patientContextValue = {
+    searchString,
+    setSearchString,
+    searchExpression,
+    setSearchExpression,
+    addNewSearchExp,
+    currentFilter,
+    setCurrentFilter,
+    fetchPatientData,
+    patientList,
+    tagObj,
+    isCooldown,
+    displayData,
+  };
+
+  return (
+    <PatientContext.Provider value={patientContextValue}>
+      {children}
+    </PatientContext.Provider>
+  );
+};
