32a019c6a473789002634cdae5fbdd7c9c306514
fatihis99@hotmail.com <isk.fatih1@gmail.com>
2021-10-29 15:30:42 +0300

    Add Helpers

---
+++ utils/helpers/index.js
@@ -0,0 +1,14 @@
+export const parseAddress = obj => {
+  var address = obj.city + ', ' + obj.line[0];
+  return address;
+};
+
+export const encodeQueryData = data => {
+  const ret = [];
+  for (let d in data) {
+    if (data[d] != '') {
+      ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d] + '*'));
+    }
+  }
+  return ret.join('&');
+};
