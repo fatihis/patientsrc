0c28746198ab1c12f8a5167a933c5a35439ebb86
fatihis99@hotmail.com <isk.fatih1@gmail.com>
2021-10-29 15:28:24 +0300

    Add ListItem component

--- components/atoms/ListItem/index.js
+++ components/atoms/ListItem/index.js
@@ -1,11 +1,54 @@
 import React from 'react';
-import {Text, TouchableOpacity} from 'react-native';
+import {Text, TouchableOpacity, View} from 'react-native';
 import {styles} from './styles';
 
-export const Item = ({item, onPress, textColor}) => {
+export const Item = ({item, onPress, textColor, backgroundColor}) => {
   return (
-    <TouchableOpacity onPress={onPress} style={[styles.item]}>
-      <Text style={[styles.title, textColor]}>{item.title}</Text>
+    <TouchableOpacity
+      onPress={onPress}
+      style={[styles.itemContainer, backgroundColor]}>
+      <View style={styles.itemRow}>
+        <Text
+          style={[
+            styles.itemColumn,
+            styles.flexOne,
+            textColor,
+          ]}>{`Name: ${item.name}`}</Text>
+        <Text
+          style={[
+            styles.itemColumn,
+            styles.flexTwo,
+            textColor,
+          ]}>{`Family Name: ${item.familyName}`}</Text>
+        <Text
+          style={[
+            styles.itemColumn,
+            styles.flexOne,
+            textColor,
+          ]}>{`ID: ${item.nationalId}`}</Text>
+      </View>
+      <View style={styles.itemRow}>
+        <Text
+          style={[
+            styles.itemColumn,
+            styles.flexOne,
+
+            textColor,
+          ]}>{`Address: ${item.address}`}</Text>
+      </View>
+      <View style={styles.itemRow}>
+        <Text
+          style={[
+            styles.itemColumn,
+            styles.flexOne,
+            ,
+            textColor,
+          ]}>{`Phone: ${item.phone}`}</Text>
+        <Text
+          style={[styles.itemColumn, styles.flexOne, textColor]}>{`Gender: ${
+          item.gender.charAt(0).toUpperCase() + item.gender.slice(1)
+        }`}</Text>
+      </View>
     </TouchableOpacity>
   );
 };
--- components/atoms/ListItem/styles.js
+++ components/atoms/ListItem/styles.js
@@ -1,3 +1,22 @@
 import {StyleSheet} from 'react-native';
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  itemContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  itemColumn: {
+    overflow: 'hidden',
+    textAlign: 'center',
+    display: 'flex',
+    alignSelf: 'center',
+    fontWeight: 'bold',
+  },
+  flexOne: {
+    flex: 1,
+  },
+  flexTwo: {
+    flex: 2,
+  },
+  itemRow: {display: 'flex', flexDirection: 'row', flex: 1},
+});
